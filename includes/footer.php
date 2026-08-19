07b49bb99129e598417183d070ed142cc97e9007
fabiaski <fabiaski13@gmail.com>
2026-08-18 22:47:42 -0500

    footer.php
    se agg link

--- includes/footer.php
+++ includes/footer.php
@@ -3,4 +3,7 @@
         <span> © <?= date('Y') ?> Inventario - Fabiaski <br> </span>
         <span> Ingeniería de Sistemas.</span>
     </div>
+      Diseñado por <a href="https://www.linkedin.com/in/fabian-gonzalez-7a24a7181/" target="_blank">Fabiaski</a>
+
+    
 </footer>
