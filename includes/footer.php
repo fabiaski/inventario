e5093c7adeee4f2baa1636208733aa70b7a7032d
fabiaski <fabiaski13@gmail.com>
2026-09-13 18:29:16 -0500

    ELIMINACON DE CARPTAS

--- includes/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="admin-footer">
-    <div class="container-fluid px-3 px-lg-4">
-        <span> © <?= date('Y') ?> Inventario - Fabiaski <br> </span>
-        <span> Ingeniería de Sistemas.</span>
-    </div>
-      Diseñado por <a href="https://www.linkedin.com/in/fabian-gonzalez-7a24a7181/" target="_blank">Fabiaski</a>
-
-    
-</footer>
